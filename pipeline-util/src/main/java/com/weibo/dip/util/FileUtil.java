177b55e95a965d9648828d2812351e76571f35bb
李洪勋 <hongxun@staff.weibo.com>
2018-08-13 11:49:06 +0800

    add kv replacer mapping

---
+++ pipeline-util/src/main/java/com/weibo/dip/util/FileUtil.java
@@ -0,0 +1,52 @@
+package com.weibo.dip.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 缓存文件key和value
+ * Create by hongxun on 2018/8/13
+ */
+public class FileUtil {
+
+  private static Map<String, Map<String, String>> fileCacheMap = new HashMap<>();
+
+  // todo: guava cache file kv
+  public static String getCacheValue(String fileName, String key) {
+    if (fileCacheMap.containsKey(fileName)) {
+      return fileCacheMap.get(fileName).get(key);
+    }
+    return null;
+  }
+
+  public static void cacheFile(String fileName, String split) throws IOException {
+    InputStream resourceAsStream = FileUtil.class.getClassLoader().getResourceAsStream(fileName);
+
+    BufferedReader reader = null;
+
+    try {
+      Map<String, String> map = new HashMap<>();
+      fileCacheMap.put(fileName, map);
+      reader = new BufferedReader(new InputStreamReader(resourceAsStream));
+
+      String line = null;
+
+      while ((line = reader.readLine()) != null) {
+        String[] splits = line.split(split);
+        if (splits.length == 2) {
+          map.put(splits[0], splits[1]);
+        }
+      }
+
+    } finally {
+      if (reader != null) {
+        reader.close();
+      }
+    }
+
+  }
+}
